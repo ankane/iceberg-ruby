5b804945e6acc434ed79195238e8f437af931ef2
Andrew Kane <andrew@ankane.org>
2026-07-12 21:55:19 -0700

    Updated style [skip ci]

--- test/catalog_test.rb
+++ test/catalog_test.rb
@@ -112,11 +112,12 @@ class CatalogTest < Minitest::Test
     skip
 
     catalog.create_table("iceberg_ruby_test.events") do |t|
-      t.bigint :a
-      t.string :b
+      t.bigint "a"
+      t.string "b"
     end
 
-    result = catalog.sql("INSERT INTO iceberg_ruby_test.events (a, b) VALUES ($1, $2), ($3, $4), ($5, $6)", [1, "one", 2, "two", 3, "three"])
+    params = [1, "one", 2, "two", 3, "three"]
+    result = catalog.sql("INSERT INTO iceberg_ruby_test.events (a, b) VALUES ($1, $2), ($3, $4), ($5, $6)", params)
     assert_equal ["count"], result.columns
     assert_equal [[3]], result.rows
 
