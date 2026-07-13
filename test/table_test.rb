4798adac36ca80b2cf1f3d2c0e2259b3e166bc9b
Andrew Kane <andrew@ankane.org>
2026-07-13 11:29:48 -0700

    Moved Polars tests to separate file [skip ci]

--- test/table_test.rb
+++ test/table_test.rb
@@ -33,9 +33,12 @@ class TableTest < Minitest::Test
   end
 
   def test_snapshots
-    df = Polars::DataFrame.new({"a" => [1, 2, 3], "b" => [4, 5, 6]})
-    table = catalog.create_table("iceberg_ruby_test.events", schema: df.schema)
-    table.append(df)
+    table =
+      catalog.create_table("iceberg_ruby_test.events") do |t|
+        t.int "a"
+        t.string "b"
+      end
+    table.append([{"a" => 1, "b" => "one"}, {"a" => 2, "b" => "two"}, {"a" => 3, "b" => "three"}])
 
     snapshots = table.snapshots
     assert_equal 1, snapshots.size
@@ -54,53 +57,7 @@ class TableTest < Minitest::Test
     assert_equal 0, snapshot[:schema_id]
   end
 
-  def test_to_polars
-    df = Polars::DataFrame.new({"a" => [1, 2, 3], "b" => [4, 5, 6]})
-    table = catalog.create_table("iceberg_ruby_test.events", schema: df.schema)
-    table.append(df)
-    assert_frame_equal df, table.to_polars.collect
-  end
-
-  def test_to_polars_snapshot_id
-    df = Polars::DataFrame.new({"a" => [1, 2, 3], "b" => [4, 5, 6]})
-    table = catalog.create_table("iceberg_ruby_test.events", schema: df.schema)
-    table.append(df)
-    snapshot_id = table.current_snapshot_id
-    table.append(df)
-    assert_frame_equal df, table.to_polars(snapshot_id:).collect
-  end
-
-  def test_to_polars_empty
-    table = catalog.create_table("iceberg_ruby_test.events") { |t| t.bigint "a" }
-    df = table.to_polars.collect
-    assert_equal ["a"], df.columns
-    assert_equal [Polars::Int64], df.dtypes
-  end
-
-  def test_to_polars_schema_changes
-    skip unless ENV["TEST_PYTHON"] && rest?
-
-    system "python3", "test/support/schema_changes.py", exception: true
-
-    table = catalog.load_table("iceberg_ruby_test.events")
-    expected = Polars::DataFrame.new({"c" => [1, 2, 3]})
-    assert_frame_equal expected, table.to_polars(_schema_changes: true).sort("c").collect
-  end
-
   def test_append
-    df = Polars::DataFrame.new([
-      Polars::Series.new("i32", [1, 2, 3], dtype: Polars::Int32),
-      Polars::Series.new("i64", [1, 2, 3], dtype: Polars::Int64),
-      Polars::Series.new("f32", [1, 2, 3], dtype: Polars::Float32),
-      Polars::Series.new("f64", [1, 2, 3], dtype: Polars::Float64),
-      Polars::Series.new("bool", [true, false, true], dtype: Polars::Boolean)
-    ])
-    table = catalog.create_table("iceberg_ruby_test.events", schema: df.schema)
-    assert_nil table.append(df)
-    assert_frame_equal df, table.to_polars.collect
-  end
-
-  def test_append_array
     table =
       catalog.create_table("iceberg_ruby_test.events") do |t|
         t.int "int"
@@ -117,31 +74,6 @@ class TableTest < Minitest::Test
     ]
     assert_nil table.append(data)
     assert_equal data, table.to_a
-  end
-
-  def test_append_column_order
-    df = Polars::DataFrame.new({"a" => [1, 2, 3], "b" => [4, 5, 6]})
-    table = catalog.create_table("iceberg_ruby_test.events", schema: df.schema)
-    table.append(df.with_columns("b", "a"))
-    assert_frame_equal df, table.to_polars.collect
-  end
-
-  def test_append_type_mismatch
-    df = Polars::DataFrame.new({"a" => [1, 2, 3]})
-    table = catalog.create_table("iceberg_ruby_test.events", schema: df.schema)
-    error = assert_raises(ArgumentError) do
-      table.append(df.cast(Polars::Float64))
-    end
-    assert_match "target schema is not superset of current schema", error.message
-  end
-
-  def test_append_missing_column
-    df = Polars::DataFrame.new({"a" => [1, 2, 3], "b" => [4, 5, 6]})
-    table = catalog.create_table("iceberg_ruby_test.events", schema: df.schema)
-    error = assert_raises(ArgumentError) do
-      table.append(df.drop("a"))
-    end
-    assert_match "target schema is not superset of current schema", error.message
   end
 
   def test_inspect
