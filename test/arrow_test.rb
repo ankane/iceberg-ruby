7e5ed69db08499120a8f596829c88d29638e3e27
Andrew Kane <andrew@ankane.org>
2026-07-21 21:26:18 -0700

    Added Arrow test [skip ci]

--- test/arrow_test.rb
+++ test/arrow_test.rb
@@ -1,6 +1,14 @@
 require_relative "test_helper"
 
 class ArrowTest < Minitest::Test
+  def test_from_arrow
+    rows = [{"a" => 1, "b" => "one"}, {"a" => 2, "b" => nil}, {"a" => nil, "b" => "three"}]
+    arr = Nanoarrow::Array.new(rows, Nanoarrow.struct({"a" => Nanoarrow.int64, "b" => Nanoarrow.string}))
+    table = catalog.create_table("events", schema: arr.schema)
+    table.append(arr)
+    assert_equal rows, table.to_a
+  end
+
   def test_to_arrow
     table = create_events
     arr = table.scan.to_arrow
